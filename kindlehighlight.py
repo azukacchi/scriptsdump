cbcfec32f4dccf7ab1b8a8221a5027ece3c22152
Azka <66941381+azukacchi@users.noreply.github.com>
2020-09-20 03:29:55 +0700

    Adding field for Note

--- kindlehighlight.py
+++ kindlehighlight.py
@@ -17,17 +17,22 @@ def main():
     judul = re.sub('[!@#$:]', '', soup.find_all('h3')[0].get_text())+'.csv'
     teks = soup.findAll('span', {'id':'highlight'})
     page = soup.findAll('span', {'id':'annotationNoteHeader'})
+    note = soup.findAll('span', {'id': 'note'})
 
     hal = []
     isiteks = []
+    isinotes = []
     
     for baris in teks:
         isiteks.append(baris.get_text())
 
     for baris in page:
         hal.append(re.findall('\d+',baris.get_text())[0])
+    
+    for baris in note:
+        isinotes.append(baris.get_text())
 
-    tabel = {'Highlight':isiteks, 'Page':hal}
+    tabel = {'Highlight':isiteks, 'Page':hal, 'Note':isinotes}
     df = pd.DataFrame(tabel)
     
     csv_path = os.path.join(BASE_DIR, judul)
