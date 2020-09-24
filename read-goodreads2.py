673381183a6ba345765c54108836e45bbd635104
Azka <66941381+azukacchi@users.noreply.github.com>
2020-09-24 18:32:27 +0700

    Navigate Goodreads book list with Selenium

---
+++ read-goodreads2.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import re
+from bs4 import BeautifulSoup
+import os.path
+from datetime import datetime
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def webdr():
+    options = Options()
+    options.headless = True
+    options.add_argument("--window-size=1920,1200")
+
+    DRIVER_PATH = ""
+    driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+    driver.get("https://www.goodreads.com/")
+
+    USERNAME = ''
+    PASSWORD = ''
+    login = driver.find_element_by_xpath("//input[@type='email']").send_keys(USERNAME)
+    password = driver.find_element_by_xpath("//input[@type='password']").send_keys(PASSWORD)
+    submit = driver.find_element_by_xpath("//input[@type='submit']").click()
+    time.sleep(15)
+
+    booklist = driver.find_element_by_xpath("//a[text()='My Books']").click()
+    time.sleep(15)
+    pagecount = driver.find_element_by_xpath("//select[@name='per_page']/option[text()='75']").click()
+    return driver.page_source
+
+def main(pagesrc):
+    
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+    soup = BeautifulSoup(pagesrc, features="html.parser") 
+
+    judul = soup.findAll('td',{'class':'field title'})
+    penulis = soup.findAll('td',{'class':'field author'})
+    tanggal = soup.findAll('td',{'class':'field date_read'})
+
+    title = []
+    author = []
+    date = []
+
+    for baris in judul:
+        title.append(baris.find('a').get_text().strip())
+    for baris in penulis:
+        full = re.split(',',baris.find('a').get_text().strip())
+        try: fullname = full[1].strip()+' '+full[0]
+        except: fullname = baris.find('a').get_text().strip()
+        author.append(fullname)
+
+    for baris in tanggal:
+        date.append(baris.find('span').get_text())
+
+    tabel = {'Title':title, 'Author':author, 'Status':'Finished', 'Progress':'', 'Book type':'', 'Highlight':'','Year':date}
+    df = pd.DataFrame(tabel)
+    date2 = []
+    for row in df.Year:
+        try: row = datetime.strptime(row, "%b %d, %Y").strftime('%m/%d/%y')
+        except:
+            try: row = datetime.strptime(row, "%b %Y").strftime('%m/01/%y')
+            except: row = '0'
+        date2.append(row)
+    df['Year'] = date2
+
+    csv_path = os.path.join(BASE_DIR, 'Goodreads.csv')
+    if os.path.exists(csv_path):
+        os.remove(csv_path)
+    df.to_csv(csv_path, index=False)
+    
+    print('fin')
+    
+    return
+
+if __name__ == '__main__':
+    page = webdr()
+    main(page)
